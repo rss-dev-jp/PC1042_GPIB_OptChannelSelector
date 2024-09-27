810935dee73fe2bd951c41ced84f27e13dcd3bdf
AYamamoto <a-yamamoto@rss-dev.co.jp>
2024-09-27 14:29:13 +0900

    実機テストによる修正

--- Source/OptChannelSelector/OptChannelSelector/Project_Code/GbibComms/GbibManager.cs
+++ Source/OptChannelSelector/OptChannelSelector/Project_Code/GbibComms/GbibManager.cs
@@ -4,10 +4,6 @@ using Ivi.Visa;
 using RssDev.Project_Code.Defines;
 using RssDev.RuntimeLog;
 using System;
-using System.Diagnostics;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace RssDev.Project_Code.GbibComms
 {
@@ -17,11 +13,6 @@ namespace RssDev.Project_Code.GbibComms
 	/// </summary>
 	public class GbibManager
 	{
-
-		/// <summary>
-		/// フッタ
-		/// </summary>
-		private const string DATA_FOOTER = "\r\n";
 
 #if NoComms
 
@@ -61,16 +52,6 @@ namespace RssDev.Project_Code.GbibComms
 		/// 戻り値要求コマンド送信フラグ
 		/// </summary>
 		private bool _isSendRequestCommand = false;
-
-		/// <summary>
-		/// 読み込み実行フラグ
-		/// </summary>
-		private bool _isReading = false;
-
-		/// <summary>
-		/// 取得した戻り値
-		/// </summary>
-		private string _readMessage = string.Empty;
 
 		/// <summary>
 		/// 接続開始
@@ -121,7 +102,7 @@ namespace RssDev.Project_Code.GbibComms
 				_sendCommand = command;
 #else
 				_session.FormattedIO.DiscardBuffers();  // 送受信バッファクリア
-				_session.FormattedIO.Write(command + DATA_FOOTER);
+				_session.FormattedIO.WriteLine(command);
 #endif
 				RuntimeLogger.Instance.Add(RuntimeLogger.Type.COMMENT, $"GBIBコマンド送信：{command}");
 			}
@@ -145,78 +126,19 @@ namespace RssDev.Project_Code.GbibComms
 				}
 
 				// 受信処理開始
-				_isReading = true;
-				_readMessage = string.Empty;
-				Task.Run(() => ReadProcess());
-
-				// 戻り値受信まで待機
-				var stopwatch = Stopwatch.StartNew();
-				while (_isReading)
+				try
 				{
-
-					Thread.Sleep(1);
-
-					if (stopwatch.ElapsedMilliseconds > timeout)
-					{
-						_isReading = false;
-						RuntimeLogger.Instance.Add(RuntimeLogger.Type.EXCEPTION, $"GBIB通信エラー：Read Timeout");
-						throw new TimeoutException("Timeout発生");
-					}
-
+					return _session.FormattedIO.ReadLine();
 				}
-
-				return _readMessage;
+				catch (Ivi.Visa.IOTimeoutException ex)
+				{
+					throw new TimeoutException("Timeout発生");
+				}
 
 			}
 
 			RuntimeLogger.Instance.Add(RuntimeLogger.Type.EXCEPTION, $"GBIB未接続時に{nameof(Read)}()メソッドを実行");
 			return string.Empty;
-
-		}
-
-		/// <summary>
-		/// 送信したコマンドに対する戻り値受信
-		/// </summary>
-		private void ReadProcess()
-		{
-
-			var message = new StringBuilder();
-			while (_isReading)
-			{
-
-#if NoComms
-
-				Thread.Sleep(100);
-
-				// CSEL:CHAN?
-				if (_sendCommand == CommandDefine.Instance.GetCommandCheckStatus())
-				{
-					message.Append("1").Append(DATA_FOOTER);
-				}
-				// *IDN?
-				else if (_sendCommand == CommandDefine.Instance.GetCommandCheckModel())
-				{
-					message.Append("OPTHUB,XXXXXXX,Ver.YY").Append(DATA_FOOTER);
-				}
-				// 戻り値がある入力コマンド
-				else
-				{
-					message.Append("Return value for Input command").Append(DATA_FOOTER);
-				}
-
-#else
-				message.Append(_session.FormattedIO.ReadString());
-#endif
-
-				var index = message.ToString().IndexOf(DATA_FOOTER);
-				if (index != -1)
-				{
-					_readMessage = message.ToString(0, index);
-					RuntimeLogger.Instance.Add(RuntimeLogger.Type.COMMENT, $"GBIB受信：{_readMessage}");
-					_isReading = false;
-				}
-
-			}
 
 		}
 
